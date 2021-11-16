43100040bbe0a21fb01454be106b93cf5ac9824f
Pratiklahamge <plahamge2@gmail.com>
2021-11-16 15:08:54 +0530

    Added:Find sum,avg,min,max and count from employeepayroll

---
+++ main/java/com/bridgelabz/EmployeePayRoll/Employee.java
@@ -0,0 +1,133 @@
+package com.bridgelabz.EmployeePayRoll;
+
+import java.util.ArrayList;
+
+public class Employee {
+	 int EmpId;
+	    String EmpName, Address, Department, EmpStart, Gender;
+		int PhoneNumber;
+	    double BasicPay, Deductions, TaxablePay, IncomeTax, NetPay;
+	    ArrayList<Employee> empList;
+
+	    public ArrayList<Employee> getEmpList() {
+	        return empList;
+	    }
+
+	    public void setEmpList(ArrayList<Employee> empList) {
+	        this.empList = empList;
+	    }
+
+	    public int getEmpId() {
+	        return EmpId;
+	    }
+
+	    public void setEmpId(int empId) {
+	        EmpId = empId;
+	    }
+
+	    public String getEmpName() {
+	        return EmpName;
+	    }
+
+	    public void setEmpName(String empName) {
+	        EmpName = empName;
+	    }
+
+	    public int getPhoneNumber() {
+	        return PhoneNumber;
+	    }
+
+	    public void setPhoneNumber(int i) {
+	        PhoneNumber = i;
+	    }
+
+	    public String getAddress() {
+	        return Address;
+	    }
+
+	    public void setAddress(String address) {
+	        Address = address;
+	    }
+
+	    public String getDepartment() {
+	        return Department;
+	    }
+
+	    public void setDepartment(String department) {
+	        Department = department;
+	    }
+
+	    public String getEmpStart() {
+	        return EmpStart;
+	    }
+
+	    public void setEmpStart(String empStart) {
+	        EmpStart = empStart;
+	    }
+
+	    public String getGender() {
+	        return Gender;
+	    }
+
+	    public void setGender(String gender) {
+	        Gender = gender;
+	    }
+
+	    public double getBasicPay() {
+	        return BasicPay;
+	    }
+
+	    public void setBasicPay(double basicPay) {
+	        BasicPay = basicPay;
+	    }
+
+	    public double getDeductions() {
+	        return Deductions;
+	    }
+
+	    public void setDeductions(double deductions) {
+	        Deductions = deductions;
+	    }
+
+	    public double getTaxablePay() {
+	        return TaxablePay;
+	    }
+
+	    public void setTaxablePay(double taxablePay) {
+	        TaxablePay = taxablePay;
+	    }
+
+	    public double getIncomeTax() {
+	        return IncomeTax;
+	    }
+
+	    public void setIncomeTax(double incomeTax) {
+	        IncomeTax = incomeTax;
+	    }
+
+	    public double getNetPay() {
+	        return NetPay;
+	    }
+
+	    public void setNetPay(double netPay) {
+	        NetPay = netPay;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "model.Employee{" +
+	                "EmpId=" + EmpId +
+	                ", EmpName='" + EmpName + '\'' +
+	                ", PhoneNumber='" + PhoneNumber + '\'' +
+	                ", Address='" + Address + '\'' +
+	                ", Department='" + Department + '\'' +
+	                ", EmpStart='" + EmpStart + '\'' +
+	                ", Gender='" + Gender + '\'' +
+	                ", BasicPay=" + BasicPay +
+	                ", Deductions=" + Deductions +
+	                ", TaxablePay=" + TaxablePay +
+	                ", IncomeTax=" + IncomeTax +
+	                ", NetPay=" + NetPay +
+	                '}';
+	    }
+	}
---
+++ main/java/com/bridgelabz/EmployeePayRoll/EmployeeException.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.EmployeePayRoll;
+
+public class EmployeeException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EmployeeException(String message) {
+		super(message);
+	}
+
+}
